c6b2acbd3c4228ec855177b74b421d886cbd261b
ambi hidalgo <ambio.pk@gmail.com>
2023-03-24 17:53:00 -0700

    Add Machines Index

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using Factory.Models;
+
+
+namespace Factory.Controllers
+
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()  
+    {
+      List<Machine> model = _db.Machines.ToList();
+      return View(model);
+    }
+  }
+}
