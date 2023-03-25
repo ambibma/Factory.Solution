d48fd1c97a5a8883c461baa44b04b24383b84036
ambi hidalgo <ambio.pk@gmail.com>
2023-03-24 18:07:51 -0700

    Add Detail Machine functionality

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -35,11 +35,16 @@ namespace Factory.Controllers
       _db.Machines.Add(machine);
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }   
+    
+    public ActionResult Details(int id)
+    {
+      Machine thisMachine = _db.Machines
+                                .Include(machine => machine.JoinEntities)
+                                .ThenInclude(join => join.Engineer)
+                                .FirstOrDefault(machine => machine.MachineId ==id);
+      return View(thisMachine);
     }
-
-    
-    
-    
 
   }
 }
